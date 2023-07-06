6552d083906d23c5d941cbc2c332445acdde1226
isabeldewhurst <136658229+isabeldewhurst@users.noreply.github.com>
2023-07-06 01:11:52 +0000

    Updates

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source "https://rubygems.org"
 
 gem "http"
+gem "json"
+gem "open-uri"
+
--- umbrella.rb
+++ umbrella.rb
@@ -1,1 +1,35 @@
-pp "howdy"
+require "http"
+require "json"
+require "open-uri"
+
+pp "What is your location?"
+
+user_location = gets.chomp
+
+pp "Checking the weather in " + user_location
+gmaps_key = ENV.fetch("GMAPS_KEY")
+
+gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{gmaps_key}"
+
+raw_gmaps_data = URI.open(gmaps_url).read
+
+parsed_gmaps_data = JSON.parse(raw_gmaps_data)
+
+results_array = parsed_gmaps_data.fetch("results")
+
+first_result_hash = results_array.at(0)
+
+geometry_hash = first_result_hash.fetch("geometry")
+
+location_hash = geometry_hash.fetch("location")
+
+latitude = location_hash.fetch("lat")
+
+longitude = location_hash.fetch("lng")
+
+pp "Your coordinates are: #{latitude}, #{longitude}"
+
+pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
+
+pirate_weather_url = "https://api.pirateweather.net/forecast/#{pirate_weather_key}/#{latitude},#{longitude}"
+
